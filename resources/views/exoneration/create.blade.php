45fcaa4d566977cdaf51fe00ee1b68c40230ce50
ziketted <affirmativegroups@gmail.com>
2022-11-27 19:35:39 +0100

    Exoneration formulaire

--- resources/views/exoneration/create.blade.php
+++ resources/views/exoneration/create.blade.php
@@ -1,17 +1,17 @@
 @include('header')
 <main id="main" class="main">
-    <section class="section align-items-center justify-content-center py-4">
-        <div class="row align-items-center justify-content-center py-4">
+    <section class="py-4 section align-items-center justify-content-center">
+        <div class="py-4 row align-items-center justify-content-center">
           <div class="col-lg-8">
             <div class="card">
               <div class="card-body">
                 <h5 class="card-title">Demande de certificat d'exoneration au FNPSS</h5>
-    
+
                 <!-- General Form Elements -->
                 <form action="{{ route('exoneration.create') }}" class="row g-3" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="col-md-8">
-                        <div class="row mb-3">
+                        <div class="mb-3 row">
                             <label for="inputNumber" class="col-sm-3 col-form-label">Lettre à la Direction du FNPSS</label>
                             <div class="col-sm-10">
                               <input class="form-control" name="lettre" type="file" id="formFile">
@@ -19,7 +19,7 @@
                           </div>
                     </div>
                     <div class="col-md-8">
-                        <div class="row mb-3">
+                        <div class="mb-3 row">
                             <label for="inputNumber" class="col-sm-10 col-form-label">Attestation des biens à exonerer</label>
                             <div class="col-sm-10">
                               <input class="form-control" name="attestation" type="file" id="formFile">
@@ -27,7 +27,7 @@
                           </div>
                     </div>
                     <div class="col-md-8">
-                        <div class="row mb-3">
+                        <div class="mb-3 row">
                             <label for="inputNumber" class="col-sm-10 col-form-label">Avis favorable du Ministère</label>
                             <div class="col-sm-10">
                               <input class="form-control" name="copie_avis" type="file" id="formFile">
@@ -35,7 +35,7 @@
                           </div>
                     </div>
                     <div class="col-md-8">
-                        <div class="row mb-3">
+                        <div class="mb-3 row">
                             <label for="inputNumber" class="col-sm-10 col-form-label">Liste de colisage</label>
                             <div class="col-sm-10">
                               <input class="form-control" name="liste_colisage" type="file" id="formFile">
@@ -43,7 +43,7 @@
                           </div>
                     </div>
                     <div class="col-md-8">
-                        <div class="row mb-3">
+                        <div class="mb-3 row">
                             <label for="inputNumber" class="col-sm-10 col-form-label">Copie du projet d'utilisation des biens</label>
                             <div class="col-sm-10">
                               <input class="form-control" name="projet" type="file" id="formFile">
@@ -51,7 +51,7 @@
                           </div>
                     </div>
                     <div class="col-md-8">
-                        <div class="row mb-3">
+                        <div class="mb-3 row">
                             <label for="inputNumber" class="col-sm-10 col-form-label">Lettre de transport</label>
                             <div class="col-sm-10">
                               <input class="form-control" name="transport" type="file" id="formFile">
@@ -60,7 +60,7 @@
                     </div>
 
                     <div class="col-md-8">
-                        <div class="row mb-3">
+                        <div class="mb-3 row">
                             <label for="inputNumber" class="col-sm-10 col-form-label">Facture d'achat</label>
                             <div class="col-sm-10">
                               <input class="form-control" name="facture" type="file" id="formFile">
@@ -69,7 +69,7 @@
                     </div>
 
                     <div class="col-md-8">
-                        <div class="row mb-3">
+                        <div class="mb-3 row">
                             <label for="inputNumber" class="col-sm-10 col-form-label">Rapport d'affectation des biens exonérés</label>
                             <div class="col-sm-10">
                               <input class="form-control" name="affectation" type="file" id="formFile">
@@ -78,7 +78,7 @@
                     </div>
 
                     <div class="col-md-8">
-                        <div class="form-floating mb-3">
+                        <div class="mb-3 form-floating">
                             <select name="type_exoneration" class="form-select" id="floatingSelect" aria-label="State">
                                 <label for="floatingSelect">Selectionner le type d'exonération</label>
                                 <option value="Importation">Importation</option>
@@ -86,25 +86,25 @@
                             </select>
                         </div>
                     </div>
-    
+
                     <div class="col-md-8">
                         <div class="form-floating">
                             <Textarea type="text" rows="50" cols="30" class="form-control" id="floatingEmail" name="commentaires"></Textarea>
                             <label for="floatingEmail">Commentaires</label>
                         </div>
                     </div>
-                
-                
+
+
                     <div class="col-sm-10">
                       <button type="submit" class="btn btn-primary">Demander le certificat d'exonération</button>
                     </div>
-                  
-    
+
+
                 </form><!-- End General Form Elements -->
-    
+
               </div>
             </div>
-    
+
           </div>
         </div>
     </section>
