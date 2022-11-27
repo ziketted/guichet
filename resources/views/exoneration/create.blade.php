8fa564fd46ae21b4a2f1ec88bfc58097c0cecaf4
Gael Baheha <bahehagael@gmail.com>
2022-11-27 18:33:19 +0100

    Exoneration form

--- resources/views/exoneration/create.blade.php
+++ resources/views/exoneration/create.blade.php
@@ -1,4 +1,103 @@
 @include('header')
+<main id="main" class="main">
+    <section class="section align-items-center justify-content-center py-4">
+        <div class="row align-items-center justify-content-center py-4">
+          <div class="col-lg-8">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Demande de certificat d'exoneration au FNPSS</h5>
+    
+                <!-- General Form Elements -->
+                <form action="{{ route('exoneration.create') }}" class="row g-3" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="col-md-8">
+                        <div class="row mb-3">
+                            <label for="inputNumber" class="col-sm-3 col-form-label">Lettre à la Direction du FNPSS</label>
+                            <div class="col-sm-10">
+                              <input class="form-control" name="lettre" type="file" id="formFile">
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="row mb-3">
+                            <label for="inputNumber" class="col-sm-10 col-form-label">Attestation des biens à exonerer</label>
+                            <div class="col-sm-10">
+                              <input class="form-control" name="attestation" type="file" id="formFile">
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="row mb-3">
+                            <label for="inputNumber" class="col-sm-10 col-form-label">Avis favorable du Ministère</label>
+                            <div class="col-sm-10">
+                              <input class="form-control" name="copie_avis" type="file" id="formFile">
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="row mb-3">
+                            <label for="inputNumber" class="col-sm-10 col-form-label">Liste de colisage</label>
+                            <div class="col-sm-10">
+                              <input class="form-control" name="liste_colisage" type="file" id="formFile">
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="row mb-3">
+                            <label for="inputNumber" class="col-sm-10 col-form-label">Copie du projet d'utilisation des biens</label>
+                            <div class="col-sm-10">
+                              <input class="form-control" name="projet" type="file" id="formFile">
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="row mb-3">
+                            <label for="inputNumber" class="col-sm-10 col-form-label">Lettre de transport</label>
+                            <div class="col-sm-10">
+                              <input class="form-control" name="transport" type="file" id="formFile">
+                            </div>
+                          </div>
+                    </div>
 
+                    <div class="col-md-8">
+                        <div class="row mb-3">
+                            <label for="inputNumber" class="col-sm-10 col-form-label">Facture d'achat</label>
+                            <div class="col-sm-10">
+                              <input class="form-control" name="facture" type="file" id="formFile">
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="row mb-3">
+                            <label for="inputNumber" class="col-sm-10 col-form-label">Rapport d'affectation des biens exonérés</label>
+                            <div class="col-sm-10">
+                              <input class="form-control" name="affectation" type="file" id="formFile">
+                            </div>
+                          </div>
+                    </div>
+    
+                    <div class="col-md-8">
+                        <div class="form-floating">
+                            <Textarea type="text" rows="50" cols="30" class="form-control" id="floatingEmail" name="commentaires"></Textarea>
+                            <label for="floatingEmail">Commentaires</label>
+                        </div>
+                    </div>
+                
+                
+                    <div class="col-sm-10">
+                      <button type="submit" class="btn btn-primary">S'enroler maintenant</button>
+                    </div>
+                  
+    
+                </form><!-- End General Form Elements -->
+    
+              </div>
+            </div>
+    
+          </div>
+        </div>
+    </section>
+    </main>
 
 @include('footer')
