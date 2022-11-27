2af5b151e19891fa9adfc9cf80d60f10c52e5307
Gael Baheha <bahehagael@gmail.com>
2022-11-27 18:24:24 +0100

    Header Modif

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -39,6 +39,7 @@
 
 <body>
   <!-- ======= Header ======= -->
+@auth
 <header id="header" class="header fixed-top d-flex align-items-center">
 
   <div class="d-flex align-items-center justify-content-between">
@@ -134,13 +135,16 @@
       <li class="nav-item dropdown pe-3">
 
         <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-          <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-          <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
+          <img src="{{ asset('assets/img/profile-img.jpg') }}" alt="Profile" class="rounded-circle">
+            <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
+          
         </a><!-- End Profile Iamge Icon -->
 
         <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
           <li class="dropdown-header">
-            <h6>{{ Auth::user()->name }}</h6>
+            <h6>
+              
+            </h6>
             
           </li>
           <li>
@@ -242,3 +246,4 @@
   </ul>
 
 </aside><!-- End Sidebar-->
+@endauth
