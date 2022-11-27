ff9eb1461fb322214ce2037f79632c11662ff3de
Gael Baheha <bahehagael@gmail.com>
2022-11-27 18:09:13 +0100

    Enrolement form

---
+++ resources/views/enrolement/create.blade.php
@@ -0,0 +1,54 @@
+@include('header')
+
+<main id="main" class="main">
+<section class="section align-items-center justify-content-center py-4">
+    <div class="row align-items-center justify-content-center py-4">
+      <div class="col-lg-8">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Enrolement au FNPSS</h5>
+
+            <!-- General Form Elements -->
+            <form action="{{ route('enrolement.create') }}" class="row g-3" >
+                @csrf
+                <div class="col-md-8">
+                    <div class="row mb-3">
+                        <label for="inputNumber" class="col-sm-3 col-form-label">Lettre</label>
+                        <div class="col-sm-10">
+                          <input class="form-control" name="lettre" type="file" id="formFile">
+                        </div>
+                      </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="row mb-3">
+                        <label for="inputNumber" class="col-sm-10 col-form-label">Bordereau des frais</label>
+                        <div class="col-sm-10">
+                          <input class="form-control" name="autre_document" type="file" id="formFile">
+                        </div>
+                      </div>
+                </div>
+
+                <div class="col-md-8">
+                    <div class="form-floating">
+                        <Textarea type="text" rows="50" cols="30" class="form-control" id="floatingEmail" name="commentaires"></Textarea>
+                        <label for="floatingEmail">Commentaires</label>
+                    </div>
+                </div>
+            
+            
+                <div class="col-sm-10">
+                  <button type="submit" class="btn btn-primary">S'enroler maintenant</button>
+                </div>
+              
+
+            </form><!-- End General Form Elements -->
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+</section>
+</main>
+
+@include('footer')
