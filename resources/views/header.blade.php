219341f84646151b2fc611e2b5dcc580cfb9fade
Gael Baheha <bahehagael@gmail.com>
2022-12-16 11:07:16 +0100

    Menu convention

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -288,6 +288,28 @@
 
                     <span class="ml-3 text-sm font-medium"> Exonerations </span>
                   </a>
+
+                  <a
+                    href=""
+                    class="flex items-center px-4 py-2 text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="w-5 h-5 opacity-75"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                      />
+                    </svg>
+
+                    <span class="ml-3 text-sm font-medium"> Conventions </span>
+                  </a>
                 </nav>
               </details>
 
