c3df1f60f437652666297950e482f7fdd4ac2f4a
Gael Baheha <bahehagael@gmail.com>
2022-12-16 13:57:01 +0100

    Update Convention Page

--- resources/views/convention/create.blade.php
+++ resources/views/convention/create.blade.php
@@ -25,23 +25,6 @@
                                     class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
                                     placeholder="Chercher les fichiers" required
                                   />
-
-                                  <span class="absolute inset-y-0 right-4 inline-flex items-center">
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      class="h-5 w-5 text-gray-400"
-                                      fill="none"
-                                      viewBox="0 0 24 24"
-                                      stroke="currentColor"
-                                    >
-                                      <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                                      />
-                                    </svg>
-                                  </span>
                                 </div>
                               </div>
                             </div>
